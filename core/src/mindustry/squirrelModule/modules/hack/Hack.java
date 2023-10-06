88fb3ce9f7fe4636f2728f8b7a91ff917b4c44d0
squi2rel <501410836@qq.com>
2023-10-06 15:08:04 +0800

    update

--- core/src/mindustry/squirrelModule/modules/hack/Hack.java
+++ core/src/mindustry/squirrelModule/modules/hack/Hack.java
@@ -63,7 +63,7 @@ public class Hack {
     public static int autoGGDelay;
     public static boolean immediatelyTurn, ignoreTurn, unitTrans, noKB, noHitbox, noSpawnKB, infDrag, immeMove, ignoreShield, voidWalk, speed, ignoreProcessor;
     public static float KBMulti, boundX, boundY, boundW, boundH, speedMulti;
-    public static boolean weaponImmeTurn, forceControl, holdFill, autoFill, allowBlue, holdFillMode;
+    public static boolean weaponImmeTurn, forceControl, holdFill, autoFill, allowBlue, holdFillMode, holdFillMode2;
     public static int holdFillInterval, holdFillMinItem, autoFillInterval, autoFillMaxCount;
     public static long lastFillTime, lastAutoFillTime;
     public static Item chosenItem = null;
@@ -108,7 +108,7 @@ public class Hack {
 
         manager.register("交互", "weaponImmeTurn", new Config("武器瞬间转向", null, changed(e -> weaponImmeTurn = e)));
         manager.register("交互", "forceControl", new Config("强制控制", null, changed(e -> forceControl = e)));
-        manager.register("交互", "holdFill", new Config("按住装填", new Element[]{new Label(""), slider("holdFill", 50f, 500f, 1f, 100f, f -> holdFillInterval = Mathf.ceil(f), 0, f -> "间隔 " + holdFillInterval + "ms"), new Label(""), slider("holdFill2", 0f, 1000f, 1f, 500f, f -> holdFillMinItem = Mathf.ceil(f), 2, f -> "核心物资下限 " + holdFillMinItem), check("holdFill", "填满", false, b -> holdFillMode = b), new Label("指定物品"), new Table(t -> ItemSelection.buildTable(null, t, content.items(), () -> chosenItem, i -> chosenItem = i))}, changed(e -> holdFill = e, c -> holdFillInterval + "ms")));
+        manager.register("交互", "holdFill", new Config("按住装填", new Element[]{new Label(""), slider("holdFill", 50f, 500f, 1f, 100f, f -> holdFillInterval = Mathf.ceil(f), 0, f -> "间隔 " + holdFillInterval + "ms"), new Label(""), slider("holdFill2", 0f, 1000f, 1f, 500f, f -> holdFillMinItem = Mathf.ceil(f), 2, f -> "核心物资下限 " + holdFillMinItem), check("holdFill", "填满", false, b -> holdFillMode = b), check("holdFill2", "核心优先", false, b -> holdFillMode2 = b), new Label("指定物品"), new Table(t -> ItemSelection.buildTable(null, t, content.items(), () -> chosenItem, i -> chosenItem = i))}, changed(e -> holdFill = e, c -> holdFillInterval + "ms")));
         manager.register("交互", "autoFill", new Config("自动装超速", new Element[]{new Label(""), slider("autoFill", 50f, 2000f, 1f, 100f, f -> autoFillInterval = Mathf.ceil(f), 0, f -> "检测间隔 " + autoFillInterval + "ms"), new Label(""), slider("autoFill2", 1f, 20f, 1f, 5f, f -> autoFillMaxCount = Mathf.ceil(f), 2, f -> "每次装 " + autoFillMaxCount + " 个超速")}, changed(e -> autoFill = e, c -> autoFillInterval + "ms")));
         manager.register("交互", "allowBlue", new Config("允许蓝图", null, changed(e -> allowBlue = e)));
         initFill();
@@ -241,8 +241,8 @@ public class Hack {
         fillIndexer.put(Blocks.scorch, new Item[]{Items.pyratite, Items.coal});
         fillIndexer.put(Blocks.salvo, new Item[]{Items.thorium, Items.copper, Items.silicon});
         fillIndexer.put(Blocks.scatter, new Item[]{Items.metaglass, Items.lead, Items.scrap});
-        fillIndexer.put(Blocks.foreshadow, new Item[]{Items.surgeAlloy});
         fillIndexer.put(Blocks.spectre, new Item[]{Items.thorium, Items.graphite, Items.pyratite});
+        fillIndexer.put(Blocks.breach, new Item[]{Items.tungsten, Items.beryllium});
         Item[] allItems = content.items().toArray(Item.class);
         Events.run(EventType.Trigger.update, () -> {
             try {
@@ -358,20 +358,29 @@ public class Hack {
         if (amount == 0) return false;
         if (unit.stack.amount != 0 && unit.stack.item == item) return true;
         boolean[] get = {false};
-        if (!(dst.block instanceof StorageBlock)) {
+        Runnable cont = () -> {
             indexer.eachBlock(unit, itemTransferRange, b -> b != dst && b.block instanceof StorageBlock && b.items != null && (amount == -1 ? b.items.has(item) : b.items.has(item, amount)), b -> {
                 if (get[0]) return;
                 if (unit.stack.amount != 0) dropItem(core, len);
                 Call.requestItem(player, b, item, amount == -1 ? b.items.get(item) : amount);
                 get[0] = true;
             });
-        }
-        if (!get[0] && core != null) {
-            if (holdFillMinItem == 0 ? core.items.has(item) : core.items.has(item, holdFillMinItem)) {
-                if (unit.stack.amount != 0) dropItem(core, len);
-                Call.requestItem(player, core, item, amount == -1 ? unit.type.itemCapacity : amount);
-                get[0] = true;
+        };
+        Runnable co = () -> {
+            if (!get[0] && core != null) {
+                if (holdFillMinItem == 0 ? core.items.has(item) : core.items.has(item, holdFillMinItem)) {
+                    if (unit.stack.amount != 0) dropItem(core, len);
+                    Call.requestItem(player, core, item, amount == -1 ? unit.type.itemCapacity : amount);
+                    get[0] = true;
+                }
             }
+        };
+        if (holdFillMode2 || dst.block instanceof StorageBlock) {
+            co.run();
+            cont.run();
+        } else {
+            cont.run();
+            co.run();
         }
         return get[0];
     }
