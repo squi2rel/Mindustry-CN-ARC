a40f8521584c59045f7a634444dac523198c8d5f
squi2rel <501410836@qq.com>
2024-10-24 22:42:10 +0800

    update

--- core/src/mindustry/ui/fragments/ChatFragment.java
+++ core/src/mindustry/ui/fragments/ChatFragment.java
@@ -209,16 +209,17 @@ public class ChatFragment extends Table{
             if (message.startsWith("..")) message = message.substring(1);
         }
 
-        if (Hack.customChat && !message.startsWith("/")) {
-            message = Hack.customChatText.get(message).replace("{text}", message);
-        }
-
         //avoid sending prefix-empty messages
         if(message.isEmpty() || (message.startsWith(mode.prefix) && message.substring(mode.prefix.length()).isEmpty())) return;
 
         //if (arcMessage(message)) return;
 
         history.insert(1, message);
+
+        if (Hack.customChat && !message.startsWith("/")) {
+            message = Hack.customChatText.get(message).replace("{text}", message);
+            if (message.isEmpty()) return;
+        }
 
         Events.fire(new ClientChatEvent(message));
 
